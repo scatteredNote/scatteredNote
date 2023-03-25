b789a44057e90ca385d6d5d8121aadf201a0dd47
steveoni <steohenoni2@gmail.com>
2023-03-25 19:03:31 +0100

    add tailwindcss and react-markdown-preview

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+// next.config.js
+const removeImports = require('next-remove-imports')();
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = removeImports(nextConfig)
