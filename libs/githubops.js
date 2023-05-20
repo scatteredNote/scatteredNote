d4442ca5bad9a4e1a0cf49524bd803bcd7eca341
steveoni <steohenoni2@gmail.com>
2023-05-20 16:52:19 +0100

    add functionality to create github folder for each logged in user

---
+++ libs/githubops.js
@@ -0,0 +1,43 @@
+import { Octokit } from "@octokit/rest";
+
+
+export async function createGithubFolder(user) {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
+  const userFolder = `users/${user}/.gitkeep`;
+
+  // Check if user folder exists in repo
+  octokit.repos.getContent({
+    owner: "steveoni",
+    repo: "testnote",
+    path: userFolder,
+  })
+    .then(() => {
+      // User folder exists
+      console.log(`User folder already exists: ${userFolder}`);
+    })
+    .catch((error) => {
+      // User folder does not exist
+      if (error.status === 404) {
+        octokit.repos.createOrUpdateFileContents({
+          owner: "steveoni",
+          repo: "testnote",
+          path: userFolder,
+          message: `Create ${userFolder}/README.md`,
+          content: Buffer.from("").toString("base64"),
+        })
+          .then(() => {
+            console.log(`User folder created: ${userFolder}`);
+          })
+          .catch((error) => {
+            console.log(`Error creating user folder: ${userFolder}`);
+            console.log(error);
+          });
+      } else {
+        console.log(`Error getting user folder: ${userFolder}`);
+        console.log(error);
+      }
+    });
+}
