b227d8aafe9c66ec66dfa41bb170bc8198bb5607
steveoni <steohenoni2@gmail.com>
2023-06-23 22:31:57 +0100

    add session redirect to vscode localhost server

--- pages/api/session.js
+++ pages/api/session.js
@@ -16,5 +16,6 @@ export default async function handler(req, res) {
 
   const { user, accessToken } = session;
 
-  res.status(200).json({ username: user.name, accessToken });
+  // res.status(200).json({ username: user.username, accessToken });
+  res.redirect(`http://localhost:54321/api/${accessToken}/${user.username}`)
 }
