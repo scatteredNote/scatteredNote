d38955135a46c38e7585aef00b92dea4e5d6f2f2
steveoni <steohenoni2@gmail.com>
2023-06-25 02:38:16 +0100

    create a middleware wrappe rfunction for cors

---
+++ libs/middleware.js
@@ -0,0 +1,21 @@
+import Cors from 'cors'
+
+// Initialize the cors middleware
+export const cors = Cors({
+  origin: '*', // You can specify your allowed origins here instead of using '*'
+  methods: ['GET', 'POST'], // Specify the allowed HTTP methods
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+export function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
