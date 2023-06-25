ca8ab5c0d6f8310dabe0c761eaa5898a5ba35a1f
steveoni <steohenoni2@gmail.com>
2023-06-25 02:39:40 +0100

    add cors to api

--- pages/api/userdirectory.js
+++ pages/api/userdirectory.js
@@ -1,13 +1,16 @@
 import { generateDirectoryStructure, generateDirectoryFilese } from "../../libs/githubops"
+import { cors, runMiddleware } from "../../libs/middleware"
+
 
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors)
   const { username } = req.query;
   const owner = username; // Assuming the GitHub username is the owner of the repository
   const userDir = `/users/${username}`; // Replace with your repository name
 
   try {
-    const data = generateDirectoryStructure(userDir);
-    const directoryStructure = generateDirectoryFilese(userDir);
+    const data = await generateDirectoryStructure(userDir);
+    const directoryStructure = await generateDirectoryFilese(userDir);
     const response = { data, directoryStructure }
     res.status(200).json(response);
   } catch (error) {
