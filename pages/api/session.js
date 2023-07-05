e104b2926c82508694b7e16e324021eedf113036
steveoni <steohenoni2@gmail.com>
2023-07-05 22:43:32 +0100

    check if request is from chrome

--- pages/api/session.js
+++ pages/api/session.js
@@ -1,9 +1,12 @@
 import { getServerSession } from "next-auth/next"
 import { getToken, encode } from "next-auth/jwt"
 import { authOptions } from 'pages/api/auth/[...nextauth]'
+import { cors, runMiddleware } from "../../libs/middleware"
 
 
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors)
+  const { chrome } = req.query;
   const session = await getServerSession(req, res, authOptions)
 
   if (!session) {
@@ -29,5 +32,10 @@ export default async function handler(req, res) {
 
   const accessToken = await encode({ token: token, secret: process.env.NEXTAUTH_SECRET });
   // res.status(200).json({ username: user.username, accessToken });
-  res.redirect(`http://localhost:54321/api/${accessToken}/${user.username}`)
+  if (chrome) {
+    return res.status(200).json({ username: user.username, accessToken });
+  } else {
+    res.redirect(`http://localhost:54321/api/${accessToken}/${user.username}`)
+  }
+
 }
