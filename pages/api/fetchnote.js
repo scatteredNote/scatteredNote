155c08cfdbf98d3e0149338ddb360749e1155b6a
steveoni <steohenoni2@gmail.com>
2023-07-06 11:03:49 +0100

    add note fetching via api

---
+++ pages/api/fetchnote.js
@@ -0,0 +1,34 @@
+import { Octokit } from "@octokit/rest";
+import { getToken } from "next-auth/jwt"
+import { cors, runMiddleware } from "../../libs/middleware"
+
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors)
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  if (token) {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    });
+    const { user, slug } = req.query;
+    const userDir = `/users/${user}/${slug}.json`;
+    console.log(userDir);
+    try {
+      const response = await octokit.repos.getContent({
+        owner: 'scatteredNote',
+        repo: 'data',
+        path: userDir,
+      });
+
+      if (response.data.type === "file") {
+        const content = Buffer.from(response.data.content, "base64").toString("utf-8");
+        res.status(200).json(JSON.parse(content));
+      }
+    } catch (error) {
+      res.status(404).json({ success: false, error: "Not found." });
+    }
+  }
+  else {
+    res.status(401).json({ success: false, error: "Unauthorized." });
+  }
+}
