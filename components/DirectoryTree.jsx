7d37eeb514dff7e49d1899b020b30160c55badfe
steveoni <steohenoni2@gmail.com>
2023-07-14 01:41:59 +0100

    update content listing

--- components/DirectoryTree.jsx
+++ components/DirectoryTree.jsx
@@ -16,20 +16,31 @@ export default function DirectoryTree({ data, user, isChild, setContentPage }) {
   return (
     <div>
       {isDirectory && (
-        <div onClick={handleToggle} className={`${isChild ? "ml-2" : "ml-0"}`}>
-          {isExpanded ? '-' : '+'}
-          {data.name}
+        <div onClick={handleToggle} className={`flex items-center py-1 ${isChild ? "ml-2" : "ml-0"}`}>
+
+          <span className='text-md font-medium hover:text-gray-1000 text-gray-900'>{data.name}</span>
+          <span className='ml-auto w-6'>{isExpanded ? (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            </svg>
+
+          ) : (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+
+          )}</span>
         </div>
       )}
       {!isDirectory && (
-        <div>
+        <div className='ml-2 text-sm font-medium hover:text-gray-1000 text-gray-900 border-l-2'>
           <Link href={`/${user}/notes/${data.value.split(".json")[0].replaceAll("/", "_")}`} className="ml-2"><button onClick={handleLinkClick}>{data.name}</button></Link>
         </div>
       )}
       {isDirectory && isExpanded && (
         <ul>
           {data.children.map((child) => (
-            <li key={child.name}>
+            <li key={child.name} >
               <DirectoryTree data={child} user={user} isChild={true} setContentPage={setContentPage} />
             </li>
           ))}
