b34e4641922623a62430335e54b21b4cf86ea959
steveoni <steohenoni2@gmail.com>
2023-07-25 22:43:31 +0100

    add gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,28 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <meta name="viewport" content="initial-scale=1.0, width=device-width" /> */}
+
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-42BKEPJ9FQ}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+               window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-42BKEPJ9FQ');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
