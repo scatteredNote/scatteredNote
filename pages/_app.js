896d5a9e1e6bc3c69ce925168ecf6369642ba77f
steveoni <steohenoni2@gmail.com>
2023-07-29 22:45:16 +0100

    add next-seo

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,27 @@ import '@/styles/globals.css';
 import '@/styles/files.scss';
 import { SessionProvider } from "next-auth/react"
 import "@fontsource/manrope";
+import { DefaultSeo } from 'next-seo';
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
-     <Component {...pageProps} />
+      <DefaultSeo title="scatteredNote" description="A Simple ui to grab contents and take note easily: organize and query your note intelligently."
+        openGraph={{
+          url: 'https://www.scatterednote.com/',
+          title: 'ScatteredNote',
+          description: 'A Simple ui to grab contents and take note easily: organize and query your note intelligently.',
+          images: [{ url: 'https://www.scatterednote.com/logox.png' },
+          ],
+          siteName: 'scatterednote',
+        }}
+        twitter={{
+          handle: '@scatterednote',
+          site: '@scatterednote',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <Component {...pageProps} />
     </SessionProvider >
-  ) ;
+  );
 }
