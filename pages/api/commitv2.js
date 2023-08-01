231edfb18a402cb6eaa639be4a6189f8f462afa4
steveoni <steohenoni2@gmail.com>
2023-08-01 23:20:05 +0100

    remove unneccassary and redundant  calls to github api

--- pages/api/commitv2.js
+++ pages/api/commitv2.js
@@ -16,44 +16,29 @@ export default async function handler(req, res) {
 
       // Create main topic directory if it doesn't exist
       const mainTopicPath = `users/${user}/${MainTopic}`;
-      await createDirectoryIfNotExists(octokit, mainTopicPath);
-
-      // Create subtopic directory if it doesn't exist
       const subTopicPath = SubTopic ? `${mainTopicPath}/${SubTopic}` : mainTopicPath;
-      await createDirectoryIfNotExists(octokit, subTopicPath);
-
-      // Create note file if it doesn't exist
       const noteFilePath = note.includes("json") ? `${subTopicPath}/${note}` : `${subTopicPath}/${note}.json`;
-      await createFileIfNotExists(octokit, noteFilePath, "[]");
-
-      // Add grab, views, and tags to note file
-      const noteFileContent = await getFileContent(octokit, noteFilePath);
-      const noteFile = JSON.parse(noteFileContent);
-      noteFile.push({ grab, views, tags });
-      await updateFileContent(octokit, noteFilePath, JSON.stringify(noteFile));
+      await createOrUpdateFileContent(octokit, noteFilePath, { grab, views, tags });
 
       // Add tags to usermeta/user.json
       const userMetaPath = `userMeta/${user}.json`;
-      const userMetaContent = await getFileContent(octokit, userMetaPath);
+      const { content: userMetaContent, sha: metasha } = await getFileContent(octokit, userMetaPath);
       const userMeta = JSON.parse(userMetaContent);
       if (!userMeta.tags) userMeta.tags = [];
       userMeta.tags = [...new Set([...userMeta?.tags, ...tags])];
-      await updateFileContent(octokit, userMetaPath, JSON.stringify(userMeta));
-
-      // Add note file path to usermeta/user.json to specify isPublic
       let noteFilePath2 = noteFilePath.split("/").slice(2);
       userMeta[noteFilePath2.join("/")] = isPublic;
-      await updateFileContent(octokit, userMetaPath, JSON.stringify(userMeta));
+      await updateFileContent(octokit, userMetaPath, metasha, JSON.stringify(userMeta));
 
       // Add website url to web/website.json
       const { websiteUrl } = req.body;
       if (websiteUrl) {
         const websitePath = `web/website.json`;
-        const websiteContent = await getFileContent(octokit, websitePath);
+        const { content: websiteContent, sha: websiteSha } = await getFileContent(octokit, websitePath);
         const website = JSON.parse(websiteContent);
         if (!website[websiteUrl]) website[websiteUrl] = [];
         website[websiteUrl].push(noteFilePath);
-        await updateFileContent(octokit, websitePath, JSON.stringify(website));
+        await updateFileContent(octokit, websitePath, websiteSha, JSON.stringify(website));
       }
 
       //revalidate is having lot of cache miss
@@ -76,52 +61,6 @@ export default async function handler(req, res) {
 
 }
 
-async function createDirectoryIfNotExists(octokit, directoryPath) {
-  try {
-    await octokit.repos.getContent({
-      owner: process.env.REPO_OWNER,
-      repo: process.env.REPO_NAME,
-      path: directoryPath,
-    });
-  } catch (error) {
-    if (error.status === 404) {
-      // Directory doesn't exist, create it
-      await octokit.repos.createOrUpdateFileContents({
-        owner: process.env.REPO_OWNER,
-        repo: process.env.REPO_NAME,
-        path: `${directoryPath}/.gitkeep`,
-        message: `Create ${directoryPath}/.gitkeep`,
-        content: Buffer.from("").toString("base64"),
-      });
-    } else {
-      throw error;
-    }
-  }
-}
-
-async function createFileIfNotExists(octokit, filePath, content) {
-  try {
-    await octokit.repos.getContent({
-      owner: process.env.REPO_OWNER,
-      repo: process.env.REPO_NAME,
-      path: filePath,
-    });
-  } catch (error) {
-    if (error.status === 404) {
-      // File doesn't exist, create it
-      await octokit.repos.createOrUpdateFileContents({
-        owner: process.env.REPO_OWNER,
-        repo: process.env.REPO_NAME,
-        path: filePath,
-        message: `Create ${filePath}`,
-        content: Buffer.from(content).toString("base64"),
-      });
-    } else {
-      throw error;
-    }
-  }
-}
-
 async function getFileContent(octokit, filePath) {
   const response = await octokit.repos.getContent({
     owner: process.env.REPO_OWNER,
@@ -130,28 +69,61 @@ async function getFileContent(octokit, filePath) {
   });
 
   const content = Buffer.from(response.data.content, "base64").toString("utf-8");
-  return content;
+  return { content, sha: response.data.sha };
 }
 
-async function updateFileContent(octokit, filePath, content) {
+async function updateFileContent(octokit, filePath, sha, content) {
   const response = await octokit.repos.createOrUpdateFileContents({
     owner: process.env.REPO_OWNER,
     repo: process.env.REPO_NAME,
     path: filePath,
     message: `Update ${filePath}`,
     content: Buffer.from(content).toString("base64"),
-    sha: await getFileSha(octokit, filePath),
+    sha: sha,
   });
 
   return response.data;
 }
 
-async function getFileSha(octokit, filePath) {
-  const response = await octokit.repos.getContent({
-    owner: process.env.REPO_OWNER,
-    repo: process.env.REPO_NAME,
-    path: filePath,
-  });
 
-  return response.data.sha;
+async function createOrUpdateFileContent(octokit, filePath, content) {
+  try {
+    const response = await octokit.repos.getContent({
+      owner: process.env.REPO_OWNER,
+      repo: process.env.REPO_NAME,
+      path: filePath,
+    });
+
+    // File exists, fetch its content and append the new data
+    const existingContent = Buffer.from(response.data.content, "base64").toString("utf-8");
+    const newContent = JSON.parse(existingContent);
+    newContent.push(content);
+
+    await octokit.repos.createOrUpdateFileContents({
+      owner: process.env.REPO_OWNER,
+      repo: process.env.REPO_NAME,
+      path: filePath,
+      message: `Update ${filePath}`,
+      content: Buffer.from(JSON.stringify(newContent)).toString("base64"),
+      sha: response.data.sha, // Provide the SHA to update the existing file
+    });
+
+    return newContent;
+  } catch (error) {
+    if (error.status === 404) {
+      // File doesn't exist, create it with the provided content
+      const initialContent = [content];
+      await octokit.repos.createOrUpdateFileContents({
+        owner: process.env.REPO_OWNER,
+        repo: process.env.REPO_NAME,
+        path: filePath,
+        message: `Create ${filePath}`,
+        content: Buffer.from(JSON.stringify(initialContent)).toString("base64"),
+      });
+
+      return initialContent;
+    } else {
+      throw error;
+    }
+  }
 }
